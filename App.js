cfbcb3b4b77692cdc8e6bb4b8f859e5606775c71
ktaekwon000 <ktaekwon000@gmail.com>
2020-05-29 23:50:07 +0800

    fix firestore user collection

--- App.js
+++ App.js
@@ -2,6 +2,15 @@ import React from "react";
 import AppContainer from "./navigation";
 import Firebase, { FirebaseProvider } from "./config/Firebase";
 
+//fix firebase issues
+import { decode, encode } from "base-64";
+if (!global.btoa) {
+  global.btoa = encode;
+}
+if (!global.atob) {
+  global.atob = decode;
+}
+
 export default function App() {
   return (
     <FirebaseProvider value={Firebase}>
