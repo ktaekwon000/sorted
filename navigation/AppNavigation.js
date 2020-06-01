ea94d5991b53f7a59de6acaad51adc33ac6f80e9
delicious-chocomint <delicious-chocomint@users.noreply.github.com>
2020-06-01 23:58:30 +0800

    enable reading from firestore + ui changes

--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,5 +1,10 @@
+import React from "react";
+import { Text, TouchableOpacity } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
+import { Provider as BlogProvider } from "../config/BlogContext";
 import SettingsScreen from "../screens/app/SettingsScreen";
+import BlogListScreen from "../screens/app/BlogListScreen";
+import { createAppContainer } from "react-navigation";
 
 const AppNavigation = createStackNavigator(
   {
@@ -7,13 +12,46 @@ const AppNavigation = createStackNavigator(
       screen: SettingsScreen,
       navigationOptions: { title: "Settings" },
     },
+    BlogList: {
+      screen: BlogListScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: "Your entries",
+        headerTitleStyle: {
+          textAlign: "center",
+          flex: 1,
+        },
+        headerRight: (
+          <TouchableOpacity
+            style={{ marginRight: 13 }}
+            onPress={() => alert("new entry")}
+          >
+            <Text style={{ color: "#007AFF" }}>New Post</Text>
+          </TouchableOpacity>
+        ),
+        headerLeft: () => (
+          <TouchableOpacity
+            style={{ marginLeft: 13 }}
+            onPress={() => navigation.navigate("Settings")}
+          >
+            <Text style={{ color: "#007AFF" }}>Settings</Text>
+          </TouchableOpacity>
+        ),
+      }),
+    },
   },
+
   {
-    initialRouteName: "Settings",
+    initialRouteName: "BlogList",
     defaultNavigationOptions: {
       title: "placeholder text",
     },
   }
 );
 
-export default AppNavigation;
+const AppContainer = createAppContainer(AppNavigation);
+
+export default () => (
+  <BlogProvider>
+    <AppContainer />
+  </BlogProvider>
+);
---
+++ screens/app/BlogListScreen.js
@@ -0,0 +1,40 @@
+import React, { useContext, useEffect, useState } from "react";
+import { Text, View, StyleSheet, FlatList } from "react-native";
+import { Button } from "react-native-elements";
+import { Context as BlogContext } from "../../config/BlogContext";
+
+const BlogListScreen = ({ navigation, firebase }) => {
+  const { state, getBlogPosts } = useContext(BlogContext);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getBlogPosts(() => setLoading(false));
+  }, []);
+
+  return loading ? (
+    <Text>loading...</Text>
+  ) : (
+    <View>
+      <FlatList
+        data={state}
+        keyExtractor={(entry) => entry.createdDate}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.title}</Text>
+            <Text>{item.content}</Text>
+            <Text>{item.createdDate}</Text>
+          </View>
+        )}
+      />
+      <Button
+        title="Go to settings"
+        type="outline"
+        onPress={() => navigation.navigate("Settings")}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default BlogListScreen;
