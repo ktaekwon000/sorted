a871fb4e46b2db7de05f592d05a28160d50a87c7
ktaekwon000 <ktaekwon000@gmail.com>
2020-06-18 06:42:48 +0800

    prototype of helplines screen

---
+++ screens/app/ContactsScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const ContactsScreen = () => {
+  return (
+    <View>
+      <Text>Contacts Screen...</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ContactsScreen;
