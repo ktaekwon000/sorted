d1a1e450f7c1b2f2df37e7ca6b1fbbd3369441c4
ktaekwon000 <ktaekwon000@gmail.com>
2020-06-18 11:36:59 +0800

    implement hamburger+headers

--- screens/app/ContactsScreen.js
+++ screens/app/ContactsScreen.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
+import { createStackNavigator } from "react-navigation-stack";
+import { Button } from "react-native-elements";
+import { Ionicons } from "@expo/vector-icons";
 
 const ContactsScreen = () => {
   return (
@@ -11,4 +14,29 @@ const ContactsScreen = () => {
 
 const styles = StyleSheet.create({});
 
-export default ContactsScreen;
+const ContactsNavigation = createStackNavigator(
+  {
+    Contacts: {
+      screen: ContactsScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: "Helplines",
+        headerLeft: () => (
+          <View style={{ flexDirection: "row" }}>
+            <Button
+              icon={<Ionicons name="md-menu" size={24} color="black" />}
+              type="clear"
+              containerStyle={{ marginLeft: 6 }}
+              titleStyle={{ fontSize: 14 }}
+              onPress={() => navigation.openDrawer()}
+            />
+          </View>
+        ),
+      }),
+    },
+  },
+  {
+    initialRouteName: "Contacts",
+  }
+);
+
+export default ContactsNavigation;
