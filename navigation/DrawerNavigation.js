b31c1740c20dcbd01d4d4259255b4809941bef68
delicious-chocomint <delicious-chocomint@users.noreply.github.com>
2020-06-24 14:24:07 +0800

    use custom contentComponent in DrawerNavigation

--- navigation/DrawerNavigation.js
+++ navigation/DrawerNavigation.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { StyleSheet, ScrollView, Text, View } from "react-native";
 import { createDrawerNavigator } from "react-navigation-drawer";
 import { createAppContainer } from "react-navigation";
 import { Ionicons } from "@expo/vector-icons";
+import SafeAreaView from "react-native-safe-area-view";
+import { DrawerItems } from "react-navigation-drawer";
+import { withFirebaseHOC } from "../config/Firebase";
 import { Provider as DiaryProvider } from "../config/DiaryContext";
 import DiaryScreens from "./AppNavigation";
 import ContactsScreen from "../screens/app/ContactsScreen";
 import StatsScreen from "../screens/app/StatsScreen";
 import SettingsScreen from "../screens/app/SettingsScreen";
+
+const CustomDrawerContentComponent = ({ firebase, ...props }) => {
+  const [name, setName] = useState("loading...");
+  const [email, setEmail] = useState("loading...");
+  async function getUserInfo() {
+    const user = await firebase.retrieveUser();
+    setName(user.displayName);
+    setEmail(user.email);
+  }
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  return (
+    <ScrollView>
+      <SafeAreaView
+        style={styles.container}
+        forceInset={{ top: "always", horizontal: "never" }}
+      >
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            marginHorizontal: 30,
+            marginVertical: 10,
+            // backgroundColor: "#DCDCDC",
+          }}
+        >
+          <Text>{`Welcome ${name}.`}</Text>
+          <Text>Have a nice day today 😊</Text>
+          {/* <Text>{`${email}`}</Text> */}
+        </View>
+        <DrawerItems {...props} />
+      </SafeAreaView>
+    </ScrollView>
+  );
+};
 
 const DrawerNavigation = createDrawerNavigator(
   {
@@ -38,8 +80,15 @@ const DrawerNavigation = createDrawerNavigator(
   },
   {
     initialRouteName: "Diary",
+    contentComponent: withFirebaseHOC(CustomDrawerContentComponent),
   }
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 const AppContainer = createAppContainer(DrawerNavigation);
 
